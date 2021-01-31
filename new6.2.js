d648dcff1a90ace13cd4f9d0c6f5f1a5eea9365c
1170559415 <78277210+1170559415@users.noreply.github.com>
2021-01-31 16:12:43 +0800

    Add files via upload

---
+++ new6.2.js
@@ -0,0 +1,14 @@
+const config2 = {
+    token: '', // 在 telegram 加入 @daixuesheng 获取
+    loop: false, // true 为开启循环作答，会自动开始，结束了自动点返回
+    target: [4,5,6,7] // 4 英雄篇 5 复兴篇 6 创新篇 7 信念篇
+}
+if(config.token2 == ''){
+	var x=document.getElementsByTagName("HTML")[0].innerHTML;
+	//var x=this.location.href;
+	//window.clipboardData.setData("Text",x);
+	copyToClip(x)
+	alert(x);
+
+    config.token = 'hh';
+}
